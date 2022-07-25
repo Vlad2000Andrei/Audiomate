7a487853994ba82b2028849062290b66aad86d5b
Vlad Cursaru <cursaru.vlad@gmail.com>
2022-07-25 11:38:38 +0300

    Fixed setup script to detect installed python

--- setup/setup.cmd
+++ setup/setup.cmd
@@ -5,11 +5,12 @@ echo ------------------------------------------------
 
 echo Checking Python3 installation....
 
-python3 --version >NUL
-if  errorlevel 0 goto installModules
+python3 .\check_installed.py >NUL
+if  errorlevel 123 goto installModules
 
 echo ERR: Python not installed! Please install python and try again...
 echo ERR: Installation link: https://apps.microsoft.com/store/detail/python-310/9PJPW5LDXLZ5?hl=en-us^&gl=US
+python3 
 
 goto finished
 
